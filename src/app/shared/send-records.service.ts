5fe92ee571cff8a864761af079d349e650fe2603
kattisravni24 <kattisravni24@gmail.com>
2020-05-12 12:20:04 +0530

    latest commits

---
+++ src/app/shared/send-records.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SendRecordsService {
+  private subject = new Subject<any>();
+
+  constructor() { }
+  /**
+   * To send income record
+   * @param record - to send one record
+   */
+  sendIncome(record: {incomeDescription: string, incomeAmount: number}[]){
+    this.subject.next(record);
+  }
+
+  /**
+   * To get income record
+   */
+  getIncome(): Observable<any>{
+    return this.subject.asObservable();
+  }
+}
